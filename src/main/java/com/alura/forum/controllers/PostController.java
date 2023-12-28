edb0a28836eecdb5695bfdf218464ba985ed6244
Edgar Martínez <edgar.danmm@gmail.com>
2023-12-27 18:46:56 -0600

    ADD - added the logic of the post request to create a new post

--- src/main/java/com/alura/forum/controllers/PostController.java
+++ src/main/java/com/alura/forum/controllers/PostController.java
@@ -1,7 +1,9 @@
 package com.alura.forum.controllers;
 
 import com.alura.forum.models.post.DataPost;
+import com.alura.forum.models.post.DataResponsePost;
 import com.alura.forum.services.PostService;
+import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -9,10 +11,18 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import javax.xml.crypto.Data;
 import java.net.URI;
 
 @RestController
 @RequestMapping("/posts")
 public class PostController {
+    @Autowired
+    PostService postService;
+
+    @PostMapping
+    public ResponseEntity<DataResponsePost> publish(@Valid @RequestBody DataPost dataPost, UriComponentsBuilder uriComponentsBuilder){
+        return new ResponseEntity(postService.publish(dataPost, uriComponentsBuilder), HttpStatus.CREATED);
+    }
 
 }
--- src/main/java/com/alura/forum/services/PostService.java
+++ src/main/java/com/alura/forum/services/PostService.java
@@ -1,17 +1,49 @@
 package com.alura.forum.services;
 
+import com.alura.forum.infra.ValidacionDeIntegridad;
+import com.alura.forum.models.course.Course;
 import com.alura.forum.models.post.DataPost;
 import com.alura.forum.models.post.DataResponsePost;
 import com.alura.forum.models.post.Post;
+import com.alura.forum.models.user.User;
+import com.alura.forum.repositories.CourseRepository;
 import com.alura.forum.repositories.PostRepository;
+import com.alura.forum.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import javax.xml.crypto.Data;
 import java.net.URI;
 
 @Service
 public class PostService {
 
+    @Autowired
+    private PostRepository postRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private CourseRepository courseRepository;
+
+    public ResponseEntity<DataResponsePost> publish(DataPost dataPost, UriComponentsBuilder uriComponentsBuilder){
+        if (!userRepository.findById(dataPost.user_id()).isPresent()){
+            throw new ValidacionDeIntegridad("Este id para el usuario no fue encontrado");
+        }
+        if (!courseRepository.findById(dataPost.user_id()).isPresent()){
+            throw new ValidacionDeIntegridad("Este id para el curso no fue encontrado");
+        }
+
+        User user = userRepository.findById(dataPost.user_id()).get();
+        Course course = courseRepository.findById(dataPost.course_id()).get();
+
+        Post post = postRepository.save(new Post(dataPost, user, course));
+        DataResponsePost dataResponsePost = new DataResponsePost(post.getId(), post.getTitle(), post.getText(), post.getStatus_post().toString(),
+                post.getAuthor().getId(), post.getCourse().getId(), post.getAnswers(), post.getPost_date());
+        URI url = uriComponentsBuilder.path("/posts/{id}").buildAndExpand(post.getId()).toUri();
+        return ResponseEntity.created(url).body(dataResponsePost);
+    }
 }
