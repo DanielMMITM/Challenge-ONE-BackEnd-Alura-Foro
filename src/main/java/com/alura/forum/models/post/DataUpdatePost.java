ecd5e4339ae2bf5f972d5ef95a1228a28071de2a
Edgar Martínez <edgar.danmm@gmail.com>
2023-12-28 23:09:55 -0600

    Setters has been added to the post model. Also the file DataUpdatePost to get the body from the request

---
+++ src/main/java/com/alura/forum/models/post/DataUpdatePost.java
@@ -0,0 +1,17 @@
+package com.alura.forum.models.post;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+
+public record DataUpdatePost(
+        @NotNull
+        Long id,
+        @NotBlank
+        String title,
+        @NotBlank
+        String text,
+        StatusPost status_post,
+        @NotNull
+        Long course_id){
+}
--- src/main/java/com/alura/forum/models/post/Post.java
+++ src/main/java/com/alura/forum/models/post/Post.java
@@ -18,6 +18,7 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 @EqualsAndHashCode(of = "id")
+@Setter
 
 public class Post {
 
