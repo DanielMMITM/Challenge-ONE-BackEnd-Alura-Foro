deff542a5af1d520c10d84b416aee1e2c6662563
Edgar Martínez <edgar.danmm@gmail.com>
2023-12-29 15:55:29 -0600

    ADD - added the logic to sign up

--- src/main/java/com/alura/forum/controllers/UserController.java
+++ src/main/java/com/alura/forum/controllers/UserController.java
@@ -1,10 +1,23 @@
 package com.alura.forum.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.alura.forum.models.user.DataSignUpUser;
+import com.alura.forum.services.UserService;
+import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
 
+    @Autowired
+    private static UserService userService;
+
+    @PostMapping
+    @Transactional
+    public ResponseEntity signUp(@Valid @RequestBody DataSignUpUser dataSignUpUser){
+        return ResponseEntity.ok(userService.signUp(dataSignUpUser));
+    }
 }
--- src/main/java/com/alura/forum/services/UserService.java
+++ src/main/java/com/alura/forum/services/UserService.java
@@ -1,2 +1,18 @@
-package com.alura.forum.services;public class UserService {
+package com.alura.forum.services;
+
+import com.alura.forum.models.user.DataSignUpUser;
+import com.alura.forum.models.user.User;
+import com.alura.forum.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private static UserRepository userRepository;
+    public String signUp(DataSignUpUser dataSignUpUser) {
+        User user = userRepository.save(new User(dataSignUpUser));
+        return "User created successfully!";
+    }
 }
