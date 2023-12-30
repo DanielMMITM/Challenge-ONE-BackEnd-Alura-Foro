751882c07e001c191cdea4859db9fc768f65b712
Edgar Martínez <edgar.danmm@gmail.com>
2023-12-29 20:29:39 -0600

    ADD - added the logic to add a response to a post

--- src/main/java/com/alura/forum/controllers/ResponseController.java
+++ src/main/java/com/alura/forum/controllers/ResponseController.java
@@ -1,2 +1,30 @@
-package com.alura.forum.controllers;public class ResponseController {
+package com.alura.forum.controllers;
+
+
+import com.alura.forum.models.post.DataResponsePost;
+import com.alura.forum.models.response.DataResponse;
+import com.alura.forum.models.response.DataResponseBody;
+import com.alura.forum.services.ResponseService;
+import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
+
+@RestController
+@RequestMapping("/responses")
+public class ResponseController {
+
+    @Autowired
+    private ResponseService responseService;
+
+    @PostMapping
+    @Transactional
+    public ResponseEntity<DataResponseBody> addResponse(@Valid @RequestBody DataResponse dataResponse){
+        return ResponseEntity.ok(responseService.addResponse(dataResponse));
+    }
 }
--- src/main/java/com/alura/forum/services/ResponseService.java
+++ src/main/java/com/alura/forum/services/ResponseService.java
@@ -1,2 +1,54 @@
-package com.alura.forum.services;public class ResponseService {
+package com.alura.forum.services;
+
+import com.alura.forum.infra.ValidacionDeIntegridad;
+import com.alura.forum.models.post.DataResponsePost;
+import com.alura.forum.models.post.Post;
+import com.alura.forum.models.response.DataResponse;
+import com.alura.forum.models.response.DataResponseBody;
+import com.alura.forum.models.response.Response;
+import com.alura.forum.models.user.User;
+import com.alura.forum.repositories.PostRepository;
+import com.alura.forum.repositories.ResponseRepository;
+import com.alura.forum.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+
+@Service
+public class ResponseService {
+    @Autowired
+    private ResponseRepository responseRepository;
+
+    @Autowired
+    PostRepository postRepository;
+
+    @Autowired
+    UserRepository userRepository;
+
+    public DataResponseBody addResponse(DataResponse dataResponse){
+        if (!postRepository.findById(dataResponse.post_id()).isPresent()){
+            throw new ValidacionDeIntegridad("Este id para el post no fue encontrado");
+        }
+        if (!userRepository.findById(dataResponse.user_id()).isPresent()){
+            throw new ValidacionDeIntegridad("Este id para el usuario no fue encontrado");
+        }
+
+        Post post = postRepository.findById(dataResponse.post_id()).get();
+
+        User user = userRepository.findById(dataResponse.user_id()).get();
+
+        Response response = responseRepository.save(new Response(dataResponse, post, user));
+        DataResponseBody dataResponseBody = new DataResponseBody(response.getId(), response.getText(), response.getSolution(),
+                response.getPost().getId(), response.getAuthor().getId(), response.getResponse_date());
+//        DataResponsePost dataResponsePost = new DataResponsePost(post.getId(), post.getTitle(), post.getText(), post.getStatus_post().toString(),
+//                post.getAuthor().getId(), post.getCourse().getId(), post.getAnswers(), post.getPost_date());
+
+//        URI url = uriComponentsBuilder.path("/posts/{id}").buildAndExpand(post.getId()).toUri();
+        return dataResponseBody;
+
+    }
+
 }
