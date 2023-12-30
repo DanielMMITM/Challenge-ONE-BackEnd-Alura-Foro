a9e3155ff3446720822ac79d6810112b1298a0ec
Edgar Martínez <edgar.danmm@gmail.com>
2023-12-30 17:16:49 -0600

    Clean up

--- src/main/java/com/alura/forum/models/post/DataPost.java
+++ src/main/java/com/alura/forum/models/post/DataPost.java
@@ -4,16 +4,12 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
 public record DataPost(
-
         @NotBlank
         String title,
-
         @NotBlank
         String text,
-
         @NotNull
         Long user_id,
-
         @NotNull
         Long course_id){
 }
