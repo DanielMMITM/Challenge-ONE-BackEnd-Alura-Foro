99bded894c93ce5c86b11009685c967f052e9f16
Edgar Martínez <edgar.danmm@gmail.com>
2023-12-29 20:28:35 -0600

    ADD - Added the values to get and set the body from the request

--- src/main/java/com/alura/forum/models/response/DataResponse.java
+++ src/main/java/com/alura/forum/models/response/DataResponse.java
@@ -1,2 +1,14 @@
-package com.alura.forum.models.response;public record DataResponse() {
+package com.alura.forum.models.response;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+public record DataResponse(
+        @NotBlank
+        String text,
+        @NotNull
+        Long post_id,
+        @NotNull
+        Long user_id
+) {
 }
--- src/main/java/com/alura/forum/models/response/DataResponseBody.java
+++ src/main/java/com/alura/forum/models/response/DataResponseBody.java
@@ -1,2 +1,13 @@
-package com.alura.forum.models.response;public record DataResponseBody() {
+package com.alura.forum.models.response;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public record DataResponseBody(
+        Long id,
+        String text,
+        Boolean solution,
+        Long post_id,
+        Long user_id,
+        LocalDateTime response_date){
 }
