6568306227bfd7ad0e1669b687dc39f87c270988
Edgar Martínez <edgar.danmm@gmail.com>
2023-12-30 23:09:48 -0600

    Clean up

--- src/main/java/com/alura/forum/models/post/DataListPosts.java
+++ src/main/java/com/alura/forum/models/post/DataListPosts.java
@@ -1,8 +1,6 @@
 package com.alura.forum.models.post;
 
 import com.alura.forum.models.response.DataResponseBody;
-import com.alura.forum.models.response.Response;
-import com.alura.forum.models.user.User;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -25,7 +23,7 @@ public record DataListPosts(
         LocalDateTime post_date){
 
     public DataListPosts(Post post){
-        this(post.getId(), post.getTitle(), post.getText(), post.getStatus_post().toString(), post.getAuthor().getId(), post.getCourse().getId(), post.getAnswers().stream().map(DataResponseBody::new).collect(Collectors.toList()), post.getPost_date());
+        this(post.getId(), post.getTitle(), post.getText(), post.getStatusPost().toString(), post.getAuthor().getId(), post.getCourse().getId(), post.getAnswers().stream().map(DataResponseBody::new).collect(Collectors.toList()), post.getPostDate());
     }
 
 }
--- src/main/java/com/alura/forum/models/response/DataResponseBody.java
+++ src/main/java/com/alura/forum/models/response/DataResponseBody.java
@@ -1,19 +1,16 @@
 package com.alura.forum.models.response;
 
-import com.alura.forum.models.post.Post;
-
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 public record DataResponseBody(
         Long id,
         String text,
         Boolean solution,
-        Long post_id,
-        Long user_id,
+        Long postId,
+        Long userId,
         LocalDateTime response_date){
 
     public DataResponseBody(Response response){
-        this(response.getId(), response.getText(), response.getSolution().booleanValue(), response.getPost().getId(), response.getAuthor().getId(), response.getResponse_date());
+        this(response.getId(), response.getText(), response.getSolution().booleanValue(), response.getPost().getId(), response.getUser().getId(), response.getResponseDate());
     }
 }
