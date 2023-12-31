574c760f0cbc8617b2dbe86b8ee1e354d30dd8d5
Edgar Martínez <edgar.danmm@gmail.com>
2023-12-31 16:46:59 -0600

    ADD - Service to handle the JWT token

--- src/main/java/com/alura/forum/services/TokenService.java
+++ src/main/java/com/alura/forum/services/TokenService.java
@@ -1,2 +1,40 @@
-package com.alura.forum.services;public class TokenService {
+package com.alura.forum.services;
+
+import com.alura.forum.models.user.User;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+public class TokenService {
+
+    private static final String SECRET_KEY = "123456789123456789123456789123456789123456789123456789";
+
+    public String getToken(UserDetails user){
+        return getToken(new HashMap<>(), user);
+    }
+
+    public String getToken(Map<String,Object> extraClaims, UserDetails user){
+        return Jwts
+                .builder()
+                .setClaims(extraClaims)
+                .setSubject(user.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis()+1000*60*24))
+                .signWith(getKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    private Key getKey(){
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
 }
