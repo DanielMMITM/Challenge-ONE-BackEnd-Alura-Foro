4f798b9c9d60575296e26f144b81aa61d085b87c
Edgar Martínez <edgar.danmm@gmail.com>
2024-01-02 00:06:16 -0600

    Add user controller documentation

--- src/main/java/com/alura/forum/controllers/UserController.java
+++ src/main/java/com/alura/forum/controllers/UserController.java
@@ -2,6 +2,8 @@ package com.alura.forum.controllers;
 
 import com.alura.forum.models.user.UserInfo;
 import com.alura.forum.services.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -15,12 +17,28 @@ public class UserController {
     private UserService userService;
 
     @GetMapping("/{id}")
+    @Operation(summary = "Retrieve the information of the current user from the database",
+            description = "The user id is send through the URL and then it shows the user information.",
+            tags = {"User"},
+            method = "GET",
+            responses = {@ApiResponse(description = "User information retrieved", responseCode = "200"),
+                    @ApiResponse(description = "Forbidden. The user doesn't have the permissions to get a properly response.", responseCode = "403")
+            }
+    )
     public ResponseEntity<UserInfo> userDetails(@PathVariable Long id){
         return new ResponseEntity(userService.userDetails(id), HttpStatus.OK);
     }
 
     @DeleteMapping("/{id}")
     @Transactional
+    @Operation(summary = "Delete the user from the database",
+            description = "The user id is send through the URL.",
+            tags = {"User"},
+            method = "DELETE",
+            responses = {@ApiResponse(description = "User deleted. It returns a string value to report that it was successfully deleted", responseCode = "200"),
+                    @ApiResponse(description = "Forbidden. The user doesn't have the permissions to get a properly response.", responseCode = "403")
+            }
+    )
     public ResponseEntity deleteUser(@PathVariable Long id){
         return ResponseEntity.ok(userService.deleteUser(id));
     }
