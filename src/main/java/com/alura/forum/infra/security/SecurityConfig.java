8730709dbf95242e3d845b384b7df636023ca639
Edgar Martínez <edgar.danmm@gmail.com>
2024-01-01 22:42:46 -0600

    Add initial documentation and permission to get acces to the url

--- src/main/java/com/alura/forum/infra/security/SecurityConfig.java
+++ src/main/java/com/alura/forum/infra/security/SecurityConfig.java
@@ -26,6 +26,7 @@ public class SecurityConfig {
                         csrf.disable())
                 .authorizeHttpRequests(authRequest ->
                         authRequest.requestMatchers(HttpMethod.POST, "/auth/**").permitAll()
+                        .requestMatchers("/swagger-ui.html", "/v3/api-docs/**", "/swagger-ui/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .sessionManagement(sessionManager->
