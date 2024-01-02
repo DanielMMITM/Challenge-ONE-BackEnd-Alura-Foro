47f7f1aa306a08fe812207cd339ba84c0ecf9636
Edgar Martínez <edgar.danmm@gmail.com>
2024-01-01 23:56:32 -0600

    Add auth controller documentation

--- src/main/java/com/alura/forum/controllers/AuthController.java
+++ src/main/java/com/alura/forum/controllers/AuthController.java
@@ -4,6 +4,8 @@ import com.alura.forum.models.user.AuthResponse;
 import com.alura.forum.models.user.DataLogInUser;
 import com.alura.forum.models.user.DataSignUpUser;
 import com.alura.forum.services.AuthService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,12 +25,28 @@ public class AuthController {
     private AuthService authService;
 
     @PostMapping(value = "login")
+    @Operation(summary = "Perform a login action to get access to the private endpoints",
+            description = "The credentials of the user are send through the body.",
+            tags = {"User->Authentication"},
+            method = "POST",
+            responses = {@ApiResponse(description = "Credentials matched", responseCode = "200"),
+                    @ApiResponse(description = "Forbidden. The user doesn't have the permissions to get a properly response.", responseCode = "403")
+            }
+    )
     public ResponseEntity<AuthResponse> authenticateUser(@RequestBody @Valid DataLogInUser dataLogInUser){
         return ResponseEntity.ok(authService.login(dataLogInUser));
     }
 
     @PostMapping(value = "signup")
     @Transactional
+    @Operation(summary = "Perform a register action. A new user is created in the database",
+            description = "The user info are send through the body so a insert query is performed into the database.",
+            tags = {"User"},
+            method = "POST",
+            responses = {@ApiResponse(description = "User Created", responseCode = "200"), @ApiResponse(description = "Bad request (missing fields)", responseCode = "400"),
+                    @ApiResponse(description = "Forbidden. The user doesn't have the permissions to get a properly response.", responseCode = "403")
+            }
+    )
     public ResponseEntity<AuthResponse> signUp(@Valid @RequestBody DataSignUpUser dataSignUpUser) {
         return ResponseEntity.ok(authService.signUp(dataSignUpUser));
     }
