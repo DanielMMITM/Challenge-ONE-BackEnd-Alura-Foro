1026721ed60ef990f0188a60588fea8c8b7075e7
Edgar Martínez <edgar.danmm@gmail.com>
2024-01-03 20:31:12 -0600

    Add params to response controller doc

--- src/main/java/com/alura/forum/controllers/ResponseController.java
+++ src/main/java/com/alura/forum/controllers/ResponseController.java
@@ -5,6 +5,8 @@ import com.alura.forum.models.response.DataResponseBody;
 import com.alura.forum.models.response.DataUpdateResponse;
 import com.alura.forum.services.ResponseService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
@@ -25,10 +27,32 @@ public class ResponseController {
                     "then the answer is inserted into the database" +
                     "and attached to the post.",
             tags = {"User->Response/Comment"},
+            parameters = {
+                @Parameter(name = "Text",
+                        description = "The text/body of the answer",
+                        required = true,
+                        example = "You should watch a tutorial",
+                        schema = @Schema(type = "String")
+                ),
+                @Parameter(name = "Post_id",
+                        description = "The id of the post that is being answered",
+                        required = true,
+                        example = "1",
+                        schema = @Schema(type = "Long")
+                ),
+                @Parameter(name = "User_id",
+                        description = "The id of the user that is answering",
+                        required = true,
+                        example = "1",
+                        schema = @Schema(type = "Long")
+                ),
+
+            },
             method = "POST",
             responses = {@ApiResponse(description = "Answer created", responseCode = "200"),
                     @ApiResponse(description = "Bad request (missing fields)", responseCode = "400"),
-                    @ApiResponse(description = "Forbidden. The user doesn't have the permissions to get a properly response.", responseCode = "403")
+                    @ApiResponse(description = "Forbidden. The user doesn't have the permissions to get a properly response.",
+                            responseCode = "403")
             }
     )
     public ResponseEntity<DataResponseBody> addResponse(@Valid @RequestBody DataResponse dataResponse){
@@ -41,10 +65,25 @@ public class ResponseController {
             description = "The user sends the information through the body and includes the post id that is watching, the id of the comment and its own id," +
                     "then the answer is updated in the database.",
             tags = {"User->Response/Comment"},
+            parameters = {
+                @Parameter(name = "Id",
+                        description = "The id of the answer to update",
+                        required = true,
+                        example = "1",
+                        schema = @Schema(type = "Long")
+                ),
+                @Parameter(name = "Text",
+                        description = "The text/body of the answer",
+                        required = true,
+                        example = "You should watch a tutorial",
+                        schema = @Schema(type = "String")
+                ),
+            },
             method = "PUT",
             responses = {@ApiResponse(description = "Answer updated", responseCode = "200"),
                     @ApiResponse(description = "Bad request (missing fields)", responseCode = "400"),
-                    @ApiResponse(description = "Forbidden. The user doesn't have the permissions to get a properly response.", responseCode = "403")
+                    @ApiResponse(description = "Forbidden. The user doesn't have the permissions to get a properly response.",
+                            responseCode = "403")
             }
     )
     public ResponseEntity<DataResponseBody> updateResponse(@Valid @RequestBody DataUpdateResponse dataUpdateResponse){
@@ -56,10 +95,20 @@ public class ResponseController {
     @Operation(summary = "Deletes the answer/comment/response in the post",
             description = "The id of the response is send through the URL and then the service perform a delete action inside the database.",
             tags = {"User->Response/Comment"},
+            parameters = {
+                    @Parameter(name = "Id",
+                            description = "The id of the answer to delete",
+                            required = true,
+                            example = "1",
+                            schema = @Schema(type = "Long")
+                    ),
+            },
             method = "DELETE",
-            responses = {@ApiResponse(description = "Answer deleted. Returns a string reporting that the answer was succesfully deleted", responseCode = "200"),
+            responses = {@ApiResponse(description = "Answer deleted. Returns a string reporting that the answer was succesfully deleted",
+                    responseCode = "200"),
                     @ApiResponse(description = "Bad request (missing fields)", responseCode = "400"),
-                    @ApiResponse(description = "Forbidden. The user doesn't have the permissions to get a properly response.", responseCode = "403")
+                    @ApiResponse(description = "Forbidden. The user doesn't have the permissions to get a properly response.",
+                            responseCode = "403")
             }
     )
     public ResponseEntity deleteResponse(@PathVariable Long id){
