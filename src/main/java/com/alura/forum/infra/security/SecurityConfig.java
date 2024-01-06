8b3a80946da71c1ec2d0a169eb3f7e09e1c63f1a
Edgar Martínez <edgar.danmm@gmail.com>
2024-01-05 20:53:00 -0600

    Configure to get 401 instead of 403 on unauthenticated requests

--- src/main/java/com/alura/forum/infra/security/SecurityConfig.java
+++ src/main/java/com/alura/forum/infra/security/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.EnableGlobalAuthentication;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -18,10 +19,14 @@ public class SecurityConfig {
     private SecurityFilter securityFilter;
     @Autowired
     private AuthenticationProvider authProvider;
+    @Autowired
+    private CustomAuthenticationEntryPoint customAuthenticationEntryPoint;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
         return httpSecurity
+                .exceptionHandling().authenticationEntryPoint(customAuthenticationEntryPoint)
+                .and()
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(authRequest -> authRequest.requestMatchers(HttpMethod.POST, "/auth/**").permitAll()
                     .requestMatchers("/swagger-ui.html", "/v3/api-docs/**", "/swagger-ui/**").permitAll()
