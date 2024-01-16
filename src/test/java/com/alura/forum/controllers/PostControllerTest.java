a6dcea01339000d76128106ba7fbdbe5c7aae21f
Edgar Martínez <edgar.danmm@gmail.com>
2024-01-16 13:42:15 -0600

    Add files for tests

---
+++ src/test/java/com/alura/forum/controllers/PostControllerTest.java
@@ -0,0 +1,2 @@
+package com.alura.forum.controllers;public class PostControllerTest {
+}
---
+++ src/test/java/com/alura/forum/repositories/UserRepositoryTest.java
@@ -0,0 +1,22 @@
+package com.alura.forum.repositories;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.ActiveProfiles;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ActiveProfiles("test")
+public class PostRepositoryTest {
+    @Autowired
+    private PostRepository postRepository;
+    @Autowired
+    private TestEntityManager em;
+
+    @Test
+    @DisplayName("It should return null when the ")
+}
